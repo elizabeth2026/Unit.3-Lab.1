d9db1466a987702e7663adee170dbc96a4f04fb9
elizabeth2026 <elizabethl2026@student.cis.edu.hk>
2023-10-03 20:26:27 +0800

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! Welcome to the jungle. Your task is to complete the game without losing");
+        System.out.println("A monkey is staring at you, suspicious. You must say a word starting with the letter 'r' and ends with 'x', and is a synonym with calm ");
+        String relax = scanner.nextLine();
+        if (relax.equals("relax")) {
+            return "true";
+        } else return "false, you lose";
+    }
+
+        }
+    }
